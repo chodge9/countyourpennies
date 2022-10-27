d9cb0798927cf53543ed9d798c3b7caf78fcb349
Hector <cuadro.bebo2326@gmail.com>
2022-10-27 10:50:44 -0500

    front end

---
+++ countyourpennies/src/components/AddGoal/AddGoal.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function AddGoal() {
+  return (
+    <div className="d-grid gap-2 mt-3">
+      <button type="addGoals" className="btn btn-primary">
+        Add a Goal
+      </button>
+    </div>
+  )
+}
+
+export default AddGoal
---
+++ countyourpennies/src/components/ViewGoals/ViewGoals.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ViewGoals() {
+  return (
+    <div className="d-grid gap-2 mt-3">
+       <button type="viewGoals" className="btn btn-primary">
+          View Your Goals
+        </button>
+    </div>
+  )
+}
+
+export default ViewGoals
