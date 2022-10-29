56003861c61c8ec6a9c1a00b57d317c79c4307a6
Jay <j.thomastt1@gmail.com>
2022-10-29 01:46:45 -0600

    trying to get nodemon to work

--- schema.js
+++ schema.js
@@ -2,7 +2,22 @@
 const graphql = Noderequire('graphql')
 const {
     GraphQLString,
-    GraphQLInt,
     GraphQLObjectType,
     GraphQLSchema
 } = graphql
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        data: {
+            type: GraphQLString,
+            resolve (_parentValue, _args) {
+                return 'Hello, Graphql!'
+            }
+        }
+    }
+})
+
+export default new GraphQLSchema({
+    query: RootQuery
+})
