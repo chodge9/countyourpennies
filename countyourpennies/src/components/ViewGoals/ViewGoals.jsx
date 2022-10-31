a25a53fae8e7fb41ed2a3b25e034cd7680fbbcc7
WeWantSomeMo <mason.oz5@yahoo.com>
2022-10-31 16:05:26 -0500

    Added Goals model and left frontend notes in Src/Comp/ViewGoals and in models/goals

--- countyourpennies/src/components/ViewGoals/ViewGoals.jsx
+++ countyourpennies/src/components/ViewGoals/ViewGoals.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 function ViewGoals() {
+  // Graph QL Query for forecasted amount/time to accrue. Depending on how often they put in money ( i.e.: Paid weekly, bi weekly, annually, etc.) it will calculate how long it should take to save.
   return (
     <div className="d-grid gap-2 mt-3">
        <button type="viewGoals" className="btn btn-primary">
