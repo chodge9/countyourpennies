cfdf770922deb1b04298fc9abc99ee0821364135
Daniel <danieltang@Daniels-MBP.attlocal.net>
2022-11-01 20:20:26 -0500

    graphql

--- countyourpennies/src/components/ViewGoals/ViewGoals.jsx
+++ countyourpennies/src/components/ViewGoals/ViewGoals.jsx
@@ -5,7 +5,7 @@ function ViewGoals() {
   return (
     <div className="d-grid gap-2 mt-3">
        <button type="viewGoals" className="btn btn-primary">
-          View Your Goals
+          View Your Goals!
         </button>
     </div>
   )
